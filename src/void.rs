211184d614aa476a673af3f9802ede164b5ffe47
Simeon <simeon.anfinrud@mac.com>
2024-10-19 15:33:15 -0700

    Make Void uninhabited and update documentation.
    The way Void was rendered uninstantiable before was by making it
    wrap a hidden private type and left it to the honor system for
    nothing to construct that private type elsewhere in the module.
    But there's a better way: an empty enum. This is how the Infallible
    type in the standard library is implemented, and enums implemented
    this way have special support as of Rust 1.82, where you can write
    `match` statements and omit branches for uninhabited types.
    
    This makes it possible to match the suspended state of a `Coro`
    whose return type is `Void` and omit the branch to handle the
    `Return` variant, making the indirection from `to_enum()` less
    necessary. However, it's still possible that using
    `SuspendedVisitor` instead of `match` in performance-critical areas
    could attain a performance improvement since it's not clear how the
    compiler represents an enum where all but one of the variants are
    uninhabited; is it smart enough to omit the discriminator? More
    research is needed.

--- src/void.rs
+++ src/void.rs
@@ -2,18 +2,47 @@ use crate::coro::Coro;
 use crate::suspended::Suspended;
 use crate::suspended::SuspendedVisitor;
 
-// Private struct to make Void uninstantiable outside this module. Do not
-// instantiate this struct. Do not derive any traits like `Default` or `Clone`.
-#[derive(Debug, PartialEq, Eq)]
-struct Uninstantiable;
-
 /// A stand-in for the 'never' type `!`, which can be used as the `Next` type
 /// for a coroutine that will never yield again.
 ///
 /// This type cannot be instantiated, so it is impossible to `Yield` from a
-/// coroutine whose `Next` type is `Void`.
+/// coroutine whose `Next` type is `Void`. Likewise, it is impossible to
+/// `Return` from a coroutine whose return type is `Void` and it is impossible
+/// to `Yield` from a coroutine whose yield type is `Void`.
+///
+/// It is useful to mark coroutines that never return as having a return type of
+/// `Void`, and coroutines that never yield as having a yield type of `Void`.
+///
+/// ```rust
+/// use cocoro::Coro;
+/// use cocoro::Void;
+/// use cocoro::from_fn;
+/// use cocoro::just_return;
+/// use cocoro::just_yield;
+///
+/// #[derive(Clone, Copy)]
+/// struct Foo;
+///
+/// // A coroutine that never yields.
+/// fn never_yield() -> impl Coro<(), Void, Foo> {
+///     just_return(Foo)
+/// }
+///
+/// // A coroutine that never returns.
+/// fn never_return() -> impl Coro<(), Foo, Void> {
+///     just_yield(Foo)
+/// }
+///
+/// // A coroutine that can never be resumed.
+/// fn never_resume() -> impl Coro<Void, (), ()> {
+///     // The input type to the closure is `Void`, so it can never be called.
+///     // Because `Void` implements `Suspended`, it can be used as the result
+///     // of the closure to meet the requirements of `from_fn()`.
+///     from_fn(|void| void)
+/// }
+/// ```
 #[derive(Debug, PartialEq, Eq)]
-pub struct Void(Uninstantiable);
+pub enum Void {}
 
 impl<T, R, I> Suspended<T, R, I> for Void {
     type Next = Void;
