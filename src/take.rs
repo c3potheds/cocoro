c47390a02a604946c3a0c1afd8d0292b2e9400d5
Simeon <simeon.anfinrud@mac.com>
2025-12-26 15:17:21 -0500

    Change take() from a combinator to a coroutine.
    This is slightly more verbose but more elegant overall; it allows
    the logic underlying take() to be used in more contexts, and it
    demonstrates the power of compose(). Furthermore, it reduces how
    often one needs to use returns::<Void>() when working with
    infinite coroutines made finite by composing with take(), and
    lets those infinite coroutines be agnostic about their return
    type and generally avoid using Void. This also gives the
    programmer more flexibility in how to assign return values for
    the case where the source returns only or the take() coroutine
    reaches its limit, using map_return() primitives as shown by the
    examples.

---
+++ src/take.rs
@@ -0,0 +1,68 @@
+use crate::FixedPointCoro;
+use crate::from_control_flow;
+use crate::with_state;
+
+/// A coroutine that yields its inputs `n` times, then returns `()`.
+///
+/// This is most commonly used as the argument to the `compose()` operator to
+/// limit how many times the source coroutine yields.
+///
+/// ```rust
+/// use cocoro::Coro;
+/// use cocoro::take;
+/// use cocoro::yield_with;
+///
+/// fn iota<R>() -> impl Coro<(), i32, R> {
+///     let mut i = 0;
+///     yield_with(move |()| {
+///         i += 1;
+///         i
+///     })
+/// }
+///
+/// // iota() by itself will continue yielding forever.
+/// iota()
+///     // Limit the yielded values to 3.
+///     .compose(take(3))
+///     .assert_yields(1, ())
+///     .assert_yields(2, ())
+///     .assert_yields(3, ())
+///     .assert_returns((), ());
+/// ```
+///
+/// If the source coroutine returns something other than `()`, you must remap
+/// the source coroutine's return type or the `take()` coroutine's return type
+/// to match the other. One easy way to do this is to lift the return value
+/// into an `Option`:
+///
+/// ```rust
+/// use cocoro::Coro;
+/// use cocoro::Yield;
+/// use cocoro::from_fn;
+/// use cocoro::just_return;
+/// use cocoro::take;
+///
+/// from_fn(|()| Yield(1, from_fn(|()| Yield(2, just_return("done")))))
+///     // Map the source coroutine's return value to `Some` to indicate that it
+///     // returned.
+///     .map_return(Some)
+///     // Map the `take()` coroutine's return value to `None` to indicate that
+///     // the limit was reached, and to match the type of the source.
+///     .compose(take(5).map_return(|_| None))
+///     .assert_yields(1, ())
+///     .assert_yields(2, ())
+///     .assert_returns(Some("done"), ());
+/// ```
+pub fn take<T>(n: usize) -> impl FixedPointCoro<T, T, ()> {
+    use core::ops::ControlFlow::*;
+    with_state(
+        n,
+        from_control_flow(|(n, t): (usize, T)| {
+            if n == 0 {
+                Break(())
+            } else {
+                Continue((n - 1, t))
+            }
+        }),
+    )
+}
